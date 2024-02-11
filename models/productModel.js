7ab70e3e931fe9f192d7b681e9684d9fca11ccdc
RavinduSM <70681852+RavinduSM@users.noreply.github.com>
2024-02-11 20:58:08 +0530

    created models for category, user and product

---
+++ models/productModel.js
@@ -0,0 +1,36 @@
+import { mongoose } from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+    name: { type: String, required: true, },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+        type: String,
+        required: true,
+    },
+},
+    { timestamps: true }
+);
+
+const productSchema = new mongoose.Schema({
+    productName: { type: String, required: true, },
+    price: { type: Number, required: true, },
+    discount: { type: Number, },
+    discountedPrice: { type: Number },
+    categories: { type: String, required: true },
+    stock: { type: Number, required: true },
+    description: { type: String, },
+    image: { type: String },
+    reviews: [reviewSchema],
+    numReviews: { type: Number, required: true, default: 0 },
+    createdAt: { type: Date, default: Date.now },
+
+},
+    {
+        timeStamps: true
+    });
+
+
+
+const Product = mongoose.model('Product', productSchema);
+export default Product;
