687262c53587452c766a6c916c8dda5375719f73
RavinduSM <ravindumuthukudaarachchi@gmail.com>
2024-02-24 10:50:28 +0530

    added authentications for product routes

--- routers/product.routes.js
+++ routers/product.routes.js
@@ -1,17 +1,25 @@
-import express from 'express';
-import { addProduct, deleteProduct, getAllProduct, getProductSearch, getSpecificProduct, productReview, updateProduct } from '../controllers/product.controller.js';
-import { authenticate } from '../middleware/authMiddleware.js';
+import express from "express";
+import {
+  addProduct,
+  deleteProduct,
+  getAllProduct,
+  getProductSearch,
+  getSpecificProduct,
+  productReview,
+  updateProduct,
+} from "../controllers/product.controller.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(authenticate, addProduct);
 router.get("/", getAllProduct);
 router.get("/:id", getSpecificProduct);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
-router.post("/:id/review", productReview);
+router.put("/:id", authenticate, updateProduct);
+router.delete("/:id", authenticate, deleteProduct);
+router.post("/:id/review", authenticate, productReview);
 router.get("/search/:key", getProductSearch);
 
-console.log()
+console.log();
 
 export default router;
